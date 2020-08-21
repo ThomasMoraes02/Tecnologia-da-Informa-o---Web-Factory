048db0480259621997bf6440f5c11269508f809c
ThomasMoraes02 <63357718+ThomasMoraes02@users.noreply.github.com>
2020-08-21 11:42:06 -0300

    Web Factory

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,7 +4,7 @@
         <p class="conteudo">Desenhe o site de sua empresa! Aqui na Web Factory damos a chance de nossos clientes serem os próprios Desenvolvedores! Desenvolva sites, blogs, anúncios... Com as ferramentas da Web Factory sua empresa estará a um passo do sucesso.</p>
 
         <div class="grid-16 imagem-intro">
-            <img src="<?php echo base_url("/assets/img/imagem2.png") ?>" alt="Web Factory">
+            <img src="<?php echo base_url("/assets/img/home/imagem2.png") ?>" alt="Web Factory">
         </div>
        
         <div class="grid-16">
@@ -23,18 +23,18 @@
         </div>
 
         <div class="grid-8">
-        <h2>Interação</h2>
-            <p>Quanto maior o alcance para que seus clientes interajam com sua empresa, maior é a chance do usuário continuar sendo seu cliente e manter a satisfação. Chat online, perguntas frequentes, espaços para que seus clientes exponham sua opinião... Essas ferramentas são de grande importancia para que alavanque seu negócio.</p>
-        </div>
-
-        <div class="grid-8">
         <h2>Satisfação</h2>
             <p>Além disso, o consumidor pode encontrar soluções em minutos e evitar a instisfação do cliente, o que consequentemente afetaria negativamente a imagem de seu negócio.</p>
         </div>
 
+        <div class="grid-8">
+        <h2>Interação</h2>
+            <p>Quanto maior o alcance para que seus clientes interajam com sua empresa, maior é a chance do usuário continuar sendo seu cliente e manter a satisfação. Chat online, perguntas frequentes, espaços para que seus clientes exponham sua opinião... Essas ferramentas são de grande importancia para que alavanque seu negócio.</p>
+        </div>
+
         <div class="grid-16">
         <h2>Vendas</h2>
-            <p>Se a empresa já está no inicio já está atuando a um tempo e quer aumentar a taxa de vendas, um site bem desenvolvido é uma das melhores escolhas, devido a facilidade que o cliente possui em realizar compras ou entrar em contato.</p>
+            <p>Se a empresa já está no inicio ou já está atuando a um tempo e quer aumentar a taxa de vendas, um site bem desenvolvido é uma das melhores escolhas, devido a facilidade que o cliente possui em realizar compras ou entrar em contato.</p>
         </div>   
 
     </div>
@@ -51,7 +51,7 @@
     <div class="container">
         <h1 class="titulo">Crie, Edite e Inove !</h1>
         <p class="conteudo">Crie sites responsivos para sua empresa, que se adapte as novas tecnologias do mercado com facilidade.</p>
-        <img src="<?php echo base_url("assets/img/imagem1.png")?>" alt="Sites Responsivos">
+        <img src="<?php echo base_url("assets/img/home/imagem1.png")?>" alt="Sites Responsivos">
     </div>
 </section>
 
